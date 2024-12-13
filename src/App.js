0b2d1c69d159ebf71c28d45175d89c9187e56060
Harshit kumar yadav <harshitkmryadav@gmail.com>
2024-12-14 00:04:26 +0530

    Added the Rough.js
    Implemented use-effect hook and href(to avoid writing links) for loading of the canvas at screen time

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-// import logo from './logo.svg';
-// import './App.css';
-
 import { useEffect, useRef } from "react";
+import rough from "roughjs";
 
 function App() {
   const canvasRef = useRef();
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const context = canvas.getContext("2d");
+
+    const roghCanvas = rough.canvas(canvas);
+    const generator = roghCanvas.generator;
+
+    context.fillStyle = "#FF0000";
+    context.fillRect(0, 0, 150, 75);
+  }, []);
   return (
     <div className="App">
-      <canvas width="100vw" height="100vh" />
+      <canvas ref={canvasRef} />
       <h1>My whiteboard App</h1>
     </div>
   );
